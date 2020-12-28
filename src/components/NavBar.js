1966f552388ee7bdbeda0085a36321f30f2ea1f3
salvaarmanasco <70273175+salvaarmanasco@users.noreply.github.com>
2020-12-28 10:10:29 -0300

    Desafio2

---
+++ src/components/NavBar.js
@@ -0,0 +1,20 @@
+import React from 'react'
+
+function NavBar(){
+    return(
+        <>
+            <header>
+                <h1>Nutrilite</h1>
+            </header>
+            <div>
+                <p>¿Quienes somos?</p>
+                <p>Productos</p>
+                <p>Historia</p>
+                <p>Contacto</p>
+                <p>Compras</p>
+            </div>
+        </>
+    )
+}
+
+export default NavBar;
