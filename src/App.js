e0ea5dd9db1df3822c867d62f0026871dab30546
salvaarmanasco <70273175+salvaarmanasco@users.noreply.github.com>
2020-12-30 14:49:33 -0300

    instalacionBootstrapFinal

--- src/App.js
+++ src/App.js
@@ -4,11 +4,13 @@ import NavBar from './components/NavBar'
 import Header from './components/Header'
 import Footer from './components/Footer'
 import ItemListContainer from './components/ItemListContainer'
+import "bootstrap/dist/css/bootstrap.min.css";
 
 
 function App() {
   return (
     <>
+      <button className="btn btn-primary ">Prueba boton</button>
       <Header/>
       <ItemListContainer greeting="Gracias por visitarnos"/>
       <NavBar/>
--- src/index.js
+++ src/index.js
@@ -3,6 +3,8 @@ import ReactDOM from 'react-dom';
 import './index.css';
 import reportWebVitals from './reportWebVitals';
 import App from './App';
+import "react-bootstrap/dist/react-bootstrap.js";
+
 
 ReactDOM.render(
     <App />,
