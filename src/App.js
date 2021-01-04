96d8cddf233e3d847b022f62d04c7436e2de9546
salvaarmanasco <70273175+salvaarmanasco@users.noreply.github.com>
2021-01-04 18:30:16 -0300

    DesafioClase5

--- src/App.js
+++ src/App.js
@@ -1,5 +1,5 @@
 import './App.css';
-import React from 'react'
+import React, {useState} from 'react'
 import NavBar from './components/NavBar'
 import Header from './components/Header'
 import Footer from './components/Footer'
@@ -7,16 +7,29 @@ import ItemListContainer from './components/ItemListContainer'
 import "bootstrap/dist/css/bootstrap.min.css";
 
 
-function App() {
+export default function App() {
+
+  const [contador, setContador] = useState(0)
+
+  const aumentarContador = () => {
+    setContador(contador + 1)
+  }
+  const restarContador = () => {
+    setContador(contador -1)
+  }
+  const resetearContador = () => {
+    setContador(0)
+  }
   return (
     <>
-      <button className="btn btn-primary ">Prueba boton</button>
       <Header/>
       <ItemListContainer greeting="Gracias por visitarnos"/>
       <NavBar/>
+      <p>Contador: { contador } </p>
+      <button className='btn-outline-success' onClick={ aumentarContador }>+</button>
+      <button className='btn-outline-warning' onClick={resetearContador} >Resetear</button>
+      <button className='btn-outline-success' onClick={restarContador}>-</button>
       <Footer/>
     </>
   );
 }
-
-export default App;
